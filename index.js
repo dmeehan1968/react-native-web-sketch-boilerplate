87ac085dd5192de4fde3cc387333b902bedc2086
Dave Meehan <dave_meehan@replicated.co.uk>
2018-01-30 17:08:05 +0000

    Add TodoApp skeleton

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
 import React from 'react'
 import { View, AppRegistry } from 'react-native'
 
+import App from './src/components/App'
 import HelloWorld from './src/components/HelloWorld'
 import { LockScreen } from './src/components/LockScreen'
 import DraggableBox from './src/components/DraggableBox'
+import LockScreenContainer from './src/components/LockScreenContainer'
 
-class LockScreenContainer extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  render() {
-    return (
-      <LockScreen
-        message="This is a sample user message.  It will change to 'unlocked' when the slider is moved all the way to the right."
-        {...this.props}
-        {...this.state}
-        onUnlock={() => {
-          this.setState({ message: 'Unlocked' })
-        }}
-      />
-    )
-  }
-}
-
+AppRegistry.registerComponent('App', () => App)
 AppRegistry.registerComponent('HelloWorld', () => HelloWorld)
 AppRegistry.registerComponent('LockScreen', () => LockScreenContainer)
 AppRegistry.registerComponent('DraggableBox', () => DraggableBox)
 
 if (window.document) {
-  AppRegistry.runApplication('LockScreen', {
+  AppRegistry.runApplication('App', {
     initialProps: {},
     rootTag: document.getElementById('app')
   });
---
+++ src/components/LockScreenContainer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { LockScreen } from './LockScreen'
+
+export default class LockScreenContainer extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <LockScreen
+        message="This is a sample user message.  It will change to 'unlocked' when the slider is moved all the way to the right."
+        {...this.props}
+        {...this.state}
+        onUnlock={() => {
+          this.setState({ message: 'Unlocked' })
+        }}
+      />
+    )
+  }
+}
