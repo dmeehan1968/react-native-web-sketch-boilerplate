d00eeb6af92fa436d535e4dc520f6697b2584ee6
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-05 19:21:05 +0000

    Make StatefulDemoApp to preserve selected item

--- src/components/Demo/index.js
+++ src/components/Demo/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SafeAreaView, TouchableOpacity, Text, View } from 'react-native'
 import { SplitNavigator } from '../Navigators'
+import PropTypes from 'prop-types'
 
 import FlatList from '../Todo/FlatList'
 import DraggableBox from '../DraggableBox'
@@ -32,119 +33,168 @@ const TextWrap = ({ viewStyle, textStyle }) => {
   )
 }
 
-export default () => (
-  <SafeAreaView
-    style={{
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'stretch',
-    }}
-  >
+class DemoApp extends React.Component {
 
-    <SplitNavigator
-      master="DemoList"
-      detail="HelloWorld"
-      views={{
-        DemoList: {
-          component: DemoList,
-          title: "Demos",
-          props: {
-            data: [
-            {
-              key: 0,
-              title: 'HelloWorld',
-              view: 'HelloWorld'
+  static propTypes = {
+    /*
+     * function to handle a demo item being pressed
+     */
+    onDemoItemPress: PropTypes.func.isRequired,
+    /*
+     * currently selected item from data
+     */
+    selected: PropTypes.object,
+  }
+
+  static defaultProps = {
+    onDemoItemPress: (demo, navigator, props, next) => { next(demo, navigator, props) }
+  }
+
+  onDemoItemPress(demo, navigator, props) {
+    navigator.navigate(demo.view, props)
+  }
+
+  render() {
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'stretch',
+        }}
+      >
+
+        <SplitNavigator
+          master="DemoList"
+          detail="HelloWorld"
+          views={{
+            DemoList: {
+              component: DemoList,
+              title: "Demos",
+              props: {
+                selected: this.props.selected,
+                data: [
+                {
+                  key: 0,
+                  title: 'HelloWorld',
+                  view: 'HelloWorld'
+                },
+                {
+                  key: 1,
+                  title: 'Draggable Box',
+                  view: 'DraggableBox'
+                },
+                {
+                  key: 2,
+                  title: 'Lock Screen',
+                  view: 'LockScreenApp'
+                },
+                {
+                  key: 3,
+                  title: 'Todo List',
+                  view: 'TodoList'
+                },
+                {
+                  key: 4,
+                  title: 'Text Wrap',
+                  view: 'TextWrap'
+                },
+                ]
+              },
+              handlers: {
+                onItemPress: (demo, navigator) => this.props.onDemoItemPress(demo, navigator, { title: demo.title }, ::this.onDemoItemPress)
+              },
             },
-            {
-              key: 1,
-              title: 'Draggable Box',
-              view: 'DraggableBox'
+            HelloWorld: {
+              component: HelloWorld,
+              title: props => props.title || 'HelloWorld',
+              props: {},
             },
-            {
-              key: 2,
-              title: 'Lock Screen',
-              view: 'LockScreenApp'
+            DraggableBox: {
+              component: DraggableBox,
+              title: props => props.title,
+              props: {},
             },
-            {
-              key: 3,
-              title: 'Todo List',
-              view: 'TodoList'
+            LockScreenApp: {
+              component: LockScreenApp,
+              title: props => props.title,
+              props: {
+                styles: {
+                  lockscreen: {
+                  }
+                }
+              },
             },
-            {
-              key: 4,
-              title: 'Text Wrap',
-              view: 'TextWrap'
+            TextWrap: {
+              component: TextWrap,
+              title: props => props.title || 'TextWrap',
+              props: {
+                viewStyle: {
+                  flex: 1,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  paddingHorizontal: 50,
+                },
+                textStyle: {
+                  textAlign: 'center',
+                }
+              },
             },
-            ]
-          },
-          handlers: {
-            onItemPress: (demo, navigator) => navigator.navigate(demo.view, { title: demo.title })
-          },
-        },
-        HelloWorld: {
-          component: HelloWorld,
-          title: props => props.title || 'HelloWorld',
-          props: {},
-        },
-        DraggableBox: {
-          component: DraggableBox,
-          title: props => props.title,
-          props: {},
-        },
-        LockScreenApp: {
-          component: LockScreenApp,
-          title: props => props.title,
-          props: {
-            styles: {
-              lockscreen: {
-              }
-            }
-          },
-        },
-        TextWrap: {
-          component: TextWrap,
-          title: props => props.title || 'TextWrap',
-          props: {
-            viewStyle: {
-              flex: 1,
-              flexDirection: 'row',
-              alignItems: 'center',
-              paddingHorizontal: 50,
+            TodoList: {
+              component: TodoList,
+              title: props => props.title,
+              props: {
+                items: [
+                { id: 0, title: 'Bread' },
+                { id: 1, title: 'Milk' },
+                { id: 2, title: 'Cheese' },
+                ]
+              },
+              handlers: {
+                onItemPress: (item, navigator) => navigator.navigate('TodoDetail', { ...item })
+              },
             },
-            textStyle: {
-              textAlign: 'center',
-            }
-          },
-        },
-        TodoList: {
-          component: TodoList,
-          title: props => props.title,
-          props: {
-            items: [
-            { id: 0, title: 'Bread' },
-            { id: 1, title: 'Milk' },
-            { id: 2, title: 'Cheese' },
-            ]
-          },
-          handlers: {
-            onItemPress: (item, navigator) => navigator.navigate('TodoDetail', { ...item })
-          },
-        },
-        TodoDetail: {
-          component: TodoDetail,
-          title: props => props.title,
-          props: {
-            style: {
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
+            TodoDetail: {
+              component: TodoDetail,
+              title: props => props.title,
+              props: {
+                style: {
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                },
+              },
             },
-          },
-        },
-      }}
-      style={{
-        flex: 1,
-      }}
-    />
-  </SafeAreaView>
-)
+          }}
+          style={{
+            flex: 1,
+          }}
+        />
+      </SafeAreaView>
+    )
+  }
+}
+
+export default class StatefulDemoApp extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: null
+    }
+  }
+
+  onDemoItemPress(demo, navigator, props, next) {
+    this.setState({ selected: demo })
+    next(demo, navigator, props)
+  }
+
+  render() {
+    return (
+      <DemoApp
+        onDemoItemPress={::this.onDemoItemPress}
+        selected={this.state.selected}
+      />
+    )
+  }
+}
