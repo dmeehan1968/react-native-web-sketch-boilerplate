1a0c09094b4fb41b5acfb29016053c14f4864d76
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-06 17:36:29 +0000

    Add PropTypes to the NavBar (#7)

--- src/components/Navigators/NavBar.js
+++ src/components/Navigators/NavBar.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import PropTypes from 'prop-types'
 
-export default ({title, backLabel, actionLabel, onBack, style}) => (
+const NavBar = ({title, backLabel, actionLabel, onBack, style}) => (
   <View style={{
     flexDirection: 'row',
     alignItems: 'center',
@@ -26,14 +27,13 @@ export default ({title, backLabel, actionLabel, onBack, style}) => (
     : null }
     <Text
       name="title"
-      style={{
+      style={[{
         fontSize: 24,
         textAlign: 'center',
         flex: 1,
         marginLeft: backLabel ? 0 : 75,
         marginRight: actionLabel ? 0 : 75,
-        ...style
-      }}
+      }, style]}
     >
       {title}
     </Text>
@@ -48,49 +48,30 @@ export default ({title, backLabel, actionLabel, onBack, style}) => (
   </View>
 )
 
-const NavBar = ({title, backLabel, actionLabel, onBack, style}) => (
-  <View style={{
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderBottomColor: '#eee',
-    borderBottomWidth: 1,
-    paddingVertical: 10,
-  }}>
-    {backLabel ?
-      <Text
-        name="backLabel"
-        style={{
-          fontSize: 30,
-          width: 75,
-          paddingLeft: 10,
-          color: 'blue'
-        }}
-        onPress={onBack}
-      >
-        {backLabel}
-      </Text>
-    : null }
-    <Text
-      name="title"
-      style={{
-        fontSize: 24,
-        textAlign: 'center',
-        flex: 1,
-        marginLeft: backLabel ? 0 : 75,
-        marginRight: actionLabel ? 0 : 75,
-        ...style
-      }}
-    >
-      {title}
-    </Text>
-    {actionLabel ?
-      <Text style={{
-        width: 75,
-        paddingRight: 10
-      }}>
-        {actionLabel}
-      </Text>
-    : null }
-  </View>
-)
+NavBar.propTypes = {
+  /*
+   * Title text for the NavBar
+   */
+  title: PropTypes.string.isRequired,
+  /*
+   * Optional label for the back button (hidden if no label)
+   */
+  backLabel: PropTypes.string,
+  /*
+   * Optional label for the action button (hidden if no label)
+   */
+  actionLabel: PropTypes.string,
+  /*
+   * Function to handle when the back button is pressed
+   */
+  onBack: PropTypes.func.isRequired,
+  /*
+   * Addtional styles to apply to the NavBar title
+   */
+  style: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.number,
+  ]),
+}
+
+export default NavBar
