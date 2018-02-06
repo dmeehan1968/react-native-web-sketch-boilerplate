82b101214fe2996fdc9b91837a0de7fd03e5338f
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-06 17:13:24 +0000

    Add PropTypes to DemoList & SelectableDemoList (#7)

--- src/components/Demo/DemoList.js
+++ src/components/Demo/DemoList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import FlatList from '../Todo/FlatList'
 import DemoItem from './DemoItem'
@@ -19,7 +20,41 @@ const DemoList = ({data, extraData, onItemPress, isItemSelected}) => (
   />
 )
 
+DemoList.propTypes = {
+  /*
+   * Array of data to that is used to populate the list
+   */
+  data: PropTypes.array.isRequired,
+  /*
+   * Extra data used to trigger render
+   */
+  extraData: PropTypes.any,
+  /*
+   * Function to handle when an item is pressed
+   */
+  onItemPress: PropTypes.func.isRequired,
+  /*
+   * Function to determine if an item is a selected item
+   */
+  isItemSelected: PropTypes.func.isRequired,
+}
+
 export default class SelectableDemoList extends React.Component {
+
+  static propTypes = {
+    /*
+     * The currently selected item
+     */
+    selected: PropTypes.any,
+    /*
+     * Array of data to that is used to populate the list
+     */
+    data: PropTypes.any,
+    /*
+     * Function to handle with an item is pressed
+     */
+    onItemPress: PropTypes.func.isRequired,
+  }
 
   constructor(props) {
     super(props)
