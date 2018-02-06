e17a2c276aa125b2bd1fa8822a8ef2adac94e63f
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-06 17:23:00 +0000

    Add comment to PropTypes for Overlay (#7)

--- src/components/LockScreen/Overlay.js
+++ src/components/LockScreen/Overlay.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import PropTypes from 'prop-types'
 
 export default class Overlay extends React.Component {
 
+  static propTypes = {
+    /*
+     * Test message to display in the overlay
+     */
+    message: PropTypes.string.isRequired,
+    /*
+     * Additional styles to apply to the overlay
+     */
+    style: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.number,
+    ]),
+  }
+  
   render() {
     return (
       <View style={{
