97ed3c7a65c3c58c2b07d49f288b8854c09ecdfb
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-07 10:11:42 +0000

    Add PropTypes to FlatList.web (#7)

--- src/components/Todo/FlatList.web.js
+++ src/components/Todo/FlatList.web.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { View } from 'react-native'
+import PropTypes from 'prop-types'
 
-export default ({
+const FlatList = ({
   data = [],
   renderItem = () => null,
   keyExtractor = () => 0
@@ -14,3 +15,24 @@ export default ({
     ))}
   </View>
 )
+
+FlatList.propTypes = {
+  /*
+   * Array of data to determine the number of items in the list
+   */
+  data: PropTypes.array,
+  /*
+   * Function that receives an item as argument and should return the
+   * rendered item
+   */
+  renderItem: PropTypes.func.isRequired,
+  /*
+   * Optional function to retrieve the component 'key' property from
+   * the item.  Receives item as an argument and should return a string.
+   * If not provided, FlatList assumes that 'data' contains an array of objects
+   * which have a 'key' property
+   */
+  keyExtractor: PropTypes.func,
+}
+
+export default FlatList
