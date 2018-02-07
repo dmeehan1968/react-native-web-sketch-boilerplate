c2fe48f3e5c6d0a5cc3a2c21e893f3ebcf76ec60
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-07 10:15:25 +0000

    Add PropTypes to TodoDetail (#7)

--- src/components/Todo/TodoDetail.js
+++ src/components/Todo/TodoDetail.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import PropTypes from 'prop-types'
 
-export default ({style, title}) => (
+const TodoDetail = ({style, title}) => (
   <View
     style={style}
   >
     <Text>
-      Content Here
+      Content Here for item '{title}'
     </Text>
   </View>
 )
+
+TodoDetail.propTypes = {
+  /*
+   * Optional styles to be passed to the detail view
+   */
+  style: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.number,
+  ]),
+  /*
+   * Title of the item as string
+   */
+  title: PropTypes.string,
+}
+
+export default TodoDetail
