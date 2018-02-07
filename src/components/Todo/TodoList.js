79afcb8c2f270bebaa400eb1ab96f9f57aa92328
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-07 11:36:29 +0000

    Add PropTypes to TodoList (#7)

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import PropTypes from 'prop-types'
 
 import FlatList from './FlatList'
 import TodoListItem from './TodoListItem'
 
-export default ({items, style, onItemPress}) => (
+const TodoList = ({items, style, onItemPress = () => {}}) => (
   <FlatList
     data={items}
     keyExtractor={item => item.id.toString()}
     renderItem={({item}) => (
       <TodoListItem
-        key={item.index}
         {...item}
         onPress={() => onItemPress(item)}
       />
@@ -18,3 +18,29 @@ export default ({items, style, onItemPress}) => (
     style={style}
   />
 )
+
+TodoList.propTypes = {
+  /*
+   * Array of todo list items
+   */
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+    }).isRequired,
+  ),
+  /*
+   * Function to handle when an item is pressed, receives the item as an
+   * argument
+   */
+  onItemPress: PropTypes.func,
+  /*
+   * Optional styles to be passed to the TodoList
+   */
+  style: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.number,
+  ]),
+}
+
+export default TodoList
