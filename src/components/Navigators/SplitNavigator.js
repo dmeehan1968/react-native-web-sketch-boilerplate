5f1416e305124a0a5ac0bef95b1d91c4a94ac94b
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-07 09:43:16 +0000

    Add PropTypes to SplitNavigator (#7)

--- src/components/Navigators/SplitNavigator.js
+++ src/components/Navigators/SplitNavigator.js
@@ -24,6 +24,75 @@ export default class SplitNavigator extends React.Component {
      * If omitted, windows over 800px in width will be in split mode
      */
     shouldSplit: PropTypes.func,
+    /*
+     * Function to handle when the split navigator mode changes.  Receives
+     * the old mode and new mode as arguments.  See SplitNavigator.DisplayMode
+     * for values
+     */
+    onModeChange: PropTypes.func,
+    /*
+     * Name of the property supplied in the views property for the initial
+     * master view
+     */
+    master: PropTypes.string.isRequired,
+    /*
+     * Name of the property supplied in the views property for the initial
+     * detail view
+     */
+    detail: PropTypes.string.isRequired,
+    /*
+     * An object containing properties that are objects describing the
+     * available views
+     */
+    views: PropTypes.objectOf(
+      PropTypes.shape({
+        /*
+         * React component to be used to render the view.  Function or class
+         */
+        component: PropTypes.func.isRequired,
+        /*
+         * The title for the NavBar
+         */
+        title: PropTypes.oneOfType([
+          /*
+           * Literal string to be used for the title
+           */
+          PropTypes.string,
+          /*
+           * Function to determine the title for the NavBar.  Receives the
+           * computed props for the component as a argument
+           */
+          PropTypes.func,
+        ]).isRequired,
+        /*
+         * Option properties object to be passed to the component
+         */
+        props: PropTypes.object,
+        /*
+         * An object containing handlers to be passed as props to the
+         * component.  This allows existing component handlers to be mapped
+         * to nvigator actions.  Receives the original arguments plus an
+         * additional 'navigator' argument, which has a 'navigate' property
+         * which is a function which takes a string which is the name of the
+         * view to navigate to (as appears in the 'views' property) and an
+         * optional object argument which are props to be passed to the view
+         *
+         * navigator = {
+         *  navigate: (view, props) => {}
+         * }
+         */
+        handlers: PropTypes.objectOf(
+          PropTypes.func,
+        ),
+      }),
+    ),
+    /*
+     * Optional styles to be passed to the view
+     */
+    style: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.number,
+    ]),
   }
 
   static defaultProps = {
