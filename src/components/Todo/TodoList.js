692eaf8009a4d2c2e5db97dce9f7572ab75069e0
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-12 16:18:18 +0000

    Remove arrow function (#16)

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -4,42 +4,68 @@ import PropTypes from 'prop-types'
 import FlatList from './FlatList'
 import TodoListItem from './TodoListItem'
 
-const TodoList = ({items, style, onItemPress = () => null}) => (
-  <FlatList
-    data={items}
-    keyExtractor={item => item.id.toString()}
-    renderItem={({item}) => (
-      <TodoListItem
-        {...item}
-        onPress={() => onItemPress(item)}
-      />
-    )}
-    style={style}
-  />
-)
+import StylePropTypes from '../StylePropTypes'
 
-TodoList.propTypes = {
-  /*
-   * Array of todo list items
-   */
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-    }).isRequired,
-  ),
-  /*
-   * Function to handle when an item is pressed, receives the item as an
-   * argument
-   */
-  onItemPress: PropTypes.func,
-  /*
-   * Optional styles to be passed to the TodoList
-   */
-  style: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.number,
-  ]),
+class TodoListItemContainer extends React.PureComponent {
+
+  static propTypes = {
+    item: PropTypes.object.isRequired,
+    onPress: PropTypes.func.isRequired,
+  }
+
+  handleItemPress = () => this.props.onPress(this.props.item)
+
+  render() {
+    return (
+      <TodoListItem
+        {...this.props.item}
+        onPress={this.handleItemPress}
+      />
+    )
+  }
 }
 
-export default TodoList
+
+export default class TodoList extends React.PureComponent {
+
+  static propTypes = {
+    /*
+     * Array of todo list items
+     */
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+      }).isRequired,
+    ),
+    /*
+     * Function to handle when an item is pressed, receives the item as an
+     * argument
+     */
+    onItemPress: PropTypes.func,
+    /*
+     * Optional styles to be passed to the TodoList
+     */
+    style: StylePropTypes({}),
+  }
+
+  keyExtractor = item => item.id.toString()
+
+  renderItem = ({item}) => (
+    <TodoListItemContainer
+      item={item}
+      onPress={this.props.onItemPress}
+    />
+  )
+
+  render() {
+    return (
+      <FlatList
+        data={this.props.items}
+        keyExtractor={this.keyExtractor}
+        renderItem={this.renderItem}
+        style={this.props.style}
+      />
+    )
+  }
+}
