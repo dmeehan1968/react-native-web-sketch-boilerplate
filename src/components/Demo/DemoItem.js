1da328e9ad217f53b01607f7369fae978dd63811
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-14 16:41:33 +0000

    Add flow support (#18)

--- src/components/Demo/DemoItem.js
+++ src/components/Demo/DemoItem.js
@@ -1,10 +1,16 @@
+// @flow
 import React from 'react'
 import { TouchableOpacity, Text, StyleSheet } from 'react-native'
-import PropTypes from 'prop-types'
 
 import designSystem from '../designSystem'
 
-const styles = StyleSheet.create({
+type Props = {
+  title: string,
+  style?: StyleSheet.StyleProp,
+  onPress: () => void
+}
+
+const styles: StyleSheet.Styles = StyleSheet.create({
   demoItem: {
     width: '100%',
     height: designSystem.list.item.height,
@@ -16,7 +22,7 @@ const styles = StyleSheet.create({
   }
 })
 
-const DemoItem = ({title, onPress, style}) => (
+const DemoItem = ({title, onPress, style}: Props) => (
   <TouchableOpacity
     onPress={onPress}
     style={[styles.demoItem, style]}
@@ -26,23 +32,5 @@ const DemoItem = ({title, onPress, style}) => (
     </Text>
   </TouchableOpacity>
 )
-
-DemoItem.propTypes = {
-  /*
-   * The text to be displayed in the item
-   */
-  title: PropTypes.string.isRequired,
-  /*
-   * Additional styles to be applied
-   */
-  style: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.number,
-  ]),
-  /*
-   * Function to call when the item is pressed
-   */
-  onPress: PropTypes.func,
-}
 
 export default DemoItem
