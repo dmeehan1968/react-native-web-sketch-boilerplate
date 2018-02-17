0818c3edf398d669d5a4cbe19fb796c0bf213052
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-17 13:27:46 +0000

    Relocate native entry point to new native folder

--- native/index.native.js
+++ native/index.native.js
@@ -1,6 +1,6 @@
 // @flow
 import { AppRegistry } from 'react-native'
 
-import DemoApp from './src/components/Demo'
+import DemoApp from '../src/components/Demo'
 
 AppRegistry.registerComponent('DemoApp', () => DemoApp)
